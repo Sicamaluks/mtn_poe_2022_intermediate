875613a2404d9953caa973f9508875c31802dc41
Sicamaluks <75622795+Sicamaluks@users.noreply.github.com>
2022-09-01 16:12:00 +0200

    replaced firebase with app

--- components/screens/Login.jsx
+++ components/screens/Login.jsx
@@ -17,12 +17,12 @@ export default function Login({ navigation }) {
 
   const onLoginPress = () => {
     // navigation.navigate("Home", { user: "Sicamaluks2014@gmail.com" });
-    firebase
+    app
       .auth()
       .signInWithEmailAndPassword(email, password)
       .then((response) => {
         const uid = response.user.uid;
-        const usersRef = firebase.firestore().collection("users");
+        const usersRef = app.firestore().collection("users");
         usersRef
           .doc(uid)
           .get()
